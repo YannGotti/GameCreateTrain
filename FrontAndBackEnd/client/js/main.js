d954fce9552ecf835dfee7328df31c4d8d81053d
MIRON <1111111111aleksey46@gmail.com>
2022-05-15 01:43:58 +0300

    add connection user + user controls
    update server + client

--- FrontAndBackEnd/client/js/main.js
+++ FrontAndBackEnd/client/js/main.js
@@ -1,21 +1,37 @@
 
 console.log(window.screen.height + ":" + window.screen.width);
 
+
+var ctx = document.getElementById('ctx').getContext("2d");
+ctx.font = '30px Arial';
+
 var socket = io();
 
+socket.on('newPositions', function(data) {
+    ctx.clearRect(0, 0, 500, 500);
+    for(var i = 0; i < data.length; i++)
+        ctx.fillText("Пидр №" + data[i].number, data[i].x, data[i].y);
+});
 
-var b = document.getElementById('button');
-var random = Math.random();
-
-
-b.onclick = function() {
-    socket.emit('happy', {
-        reason: 'yyy',
-        id: random
-    }); 
+document.onkeydown = function(event) {
+    if(event.keyCode  === 68) //d
+        socket.emit('keyPress', {inputId:'right', state:true});
+    else if(event.keyCode  === 83) //s
+        socket.emit('keyPress', {inputId:'down', state:true});
+    else if(event.keyCode  === 65) //a
+        socket.emit('keyPress', {inputId:'left', state:true});
+    else if(event.keyCode  === 87) //w
+        socket.emit('keyPress', {inputId:'up', state:true});
 };
 
+document.onkeyup = function(event) {
 
-socket.on('serverMsg', function(data){
-    console.log(data.msg);
-});
+    if(event.keyCode  === 68) //d
+        socket.emit('keyPress', {inputId:'right', state:false});
+    else if(event.keyCode  === 83) //s
+        socket.emit('keyPress', {inputId:'down', state:false});
+    else if(event.keyCode  === 65) //a
+        socket.emit('keyPress', {inputId:'left', state:false});
+    else if(event.keyCode  === 87) //w
+        socket.emit('keyPress', {inputId:'up', state:false});
+};
